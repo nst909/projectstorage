ff5b3866e7be06b50c7dbbd4a9fd27ba39876c51
hututu <qv233@qq.com>
2022-04-27 22:47:54 +0800

    Update todesk.sh

--- todesk.sh
+++ todesk.sh
@@ -1,11 +1,8 @@
 ------todesk一键安装脚本----------
 
-sudo su
+复制下面命令粘贴到终端中运行即可。
 
 cd /home/root1
-
 wget https://dl.todesk.com/linux/todesk_4.1.0_x86_64.rpm
-
 sudo rpm -Uvh todesk_4.1.0_x86_64.rpm
-
 todesk
